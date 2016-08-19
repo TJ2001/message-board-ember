3b7f0b9bf45dd76359eeb631f54d9562a72c1bf1
TJ2001 <jung.m.tim@gmail.com>
2016-08-19 16:35:40 -0700

    edit README to include more detailed information about the application

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
       question: this.store.findAll('question'),
-      answer: this.store.findAll('answer'),
+      answers: this.store.findAll('answer'),
     });
   },
 
